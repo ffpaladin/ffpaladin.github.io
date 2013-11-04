25030277dcc2f7e309ff06c72cb45f81b23e155e
ffpaladin <ffpaladin@gmail.com>
2013-11-03 16:21:44 -0800

    cleanup

--- ks/asp/export.sh
+++ ks/asp/export.sh
@@ -4,4 +4,5 @@ name="./exports/$(date +%y.%m.%d_%H.%M.%S)"
 echo $name
 grep presentation answerset.txt | sed 's/\(.*\)\([0-9]\)\(.*\)/\1 \2 \3;/' | sort -n -k 2 > $name.txt
 
+cat answerset.txt > $name.ans
 cat storyspec.txt > $name.spec
