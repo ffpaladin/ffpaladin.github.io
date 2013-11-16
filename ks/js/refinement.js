ce0c262706c6b379247f1325973c8920851ec06c
ffpaladin <ffpaladin@gmail.com>
2013-11-15 19:50:02 -0800

    changing up the parser to pass objects instead of strings

--- ks/js/refinement.js
+++ ks/js/refinement.js
@@ -1,6 +1,6 @@
 var al = {
 	name: "Al",
-	self: "himself",
+	myself: "himself",
 	pronoun: "he",
 	posessive: "his",
 	object: "him"
@@ -8,7 +8,7 @@ var al = {
 	
 var parry = {
 	name: "Parry",
-	self: "himself",
+	myself: "himself",
 	pronoun: "He",
 	posessive: "his",
 	object: "him"
@@ -16,7 +16,7 @@ var parry = {
 	
 var eliza = {
 	name: "Eliza",
-	self: "herself",
+	myself: "herself",
 	pronoun: "She",
 	posessive: "her",
 	object: "her"
@@ -29,43 +29,85 @@ var wooed = "wooed";
 var unhappy = "unhappy"
 var indifferent_tragedy = "does not care what happened to";
 var lost_friend = "lose their friend";
-var murdered = "murdered";
+var murdered = "kill";
 var kills = "kills";
 var flowers = "flowers";
-var brother = "became brothers";
+var brother = "are brothers";
 var assaulted = "assaulted";
 var sword = "sword";
 
 var captions = new Array();
 
+// transitions per page
+var capstrans = new Array();
+
+capstrans[0] = "Once upon a time...";
+capstrans[1] = "It was the case that ";
+capstrans[2] = "One day, ";
+capstrans[3] = "Then, ";
+capstrans[4] = "For whatever reason, ";
+capstrans[5] = "Which led to ";
+capstrans[6] = "As a result, ";
+capstrans[7] = "In the end, ";
+
+// Mo Objects less Strings
+
+function Sentence(object)
+{
+	this.getStr = function (){
+		return object;
+	}
+}
+
 function presentation(sentence, index){
+	
+	var s = new Sentence(sentence);
+	
 	if(captions[index] == undefined){
 		captions[index] = new Array();
-		captions[index].text = sentence + ". ";
+		captions[index].text = capstrans[index] + s.getStr() + ". ";
 	}
 	else
-      captions[index].text += sentence + ". ";
+      captions[index].text += s.getStr() + ". ";
   	return index;
 }
+
+// TOP LEVEL SENTENCES MODIFIERS: svo, attib, and motiv
 
 function svo(s, v, o){
      return(s.name + " " + v + " " + o.name);
 }
  
 function attrib(subject, attribute){
-	if (typeof attribute === 'object')
-      		return(subject.name +  " is " + attribute);
-      	else
-      		return(subject.name + " " + attribute);
+	if (attribute.indexOf(" ") !== -1)
+	{
+		return(subject.name +  " " 
+			+ attribute.replace(subject.name,subject.object));
+      	}
+	else
+      	{
+		return(subject.name + " was " + attribute);
+	}
 }
 
-function rel(relationship, person){
-     return ("and " + person.name + " " + relationship);
-}
 
 function motiv(act, actor, recipient){
-      return (actor.name + " " + act + " " + recipient.name);
+      
+      	if(actor == recipient)
+		return(actor.name + " " + act + " " + actor.myself);
+	else
+      		return (actor.name + " " + act + " " + recipient.name);
 }
+
+// SECONDARY: includes motiv (motiv has 2 uses)
+
+function rel(relationship, person){
+
+//	this.rel=relationship;
+//	this.p=person;
+	return("and " + person.name + " " + relationship);
+}
+
 
 function attacks(weapon){
 	return ("attacked with a " + weapon);
